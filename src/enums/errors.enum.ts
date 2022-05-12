1aab91d74b9cd8a330925bcb59d46267fb6d4479
Jay <jasonsaayman@gmail.com>
2022-05-12 18:45:35 +0200

    Added error enum

---
+++ src/enums/errors.enum.ts
@@ -0,0 +1,11 @@
+export enum Errors {
+  badConfig = 'ERROR_BAD_CONFIG',
+  connectionAborted = 'ERROR_CONNECTION_ABORTED',
+  timeOut = 'ERROR_TIMED_OUT',
+  networkFailure = 'ERROR_NETWORK_FAILED',
+  tooManyRedirects = 'ERROR_TOO_MANY_REDIRECTS',
+  deprecated = 'ERROR_DEPRECATED',
+  badResponse = 'ERROR_BAD_RESPONSE',
+  badRequest = 'ERROR_BAD_REQUEST',
+  canceled = 'ERROR_CANCELED',
+}
